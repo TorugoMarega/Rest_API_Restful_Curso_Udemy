bed76780fb6a7f170cbe57e8fdeff41d56c711c5
Vhmarega <victorhugomarega@hotmail.com>
2022-08-19 17:03:31 -0300

    Commit inicial

--- Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/Startup.java
+++ Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/Startup.java
@@ -8,7 +8,7 @@ public class Startup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
-		System.out.println("Hello World");
+		System.out.println("Api funcionando");
 	}
 
 }
