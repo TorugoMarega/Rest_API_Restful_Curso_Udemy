e2bf5a801474c2ca44906528d59b98dda16903c0
Vhmarega <victorhugomarega@hotmail.com>
2022-08-19 16:43:24 -0300

    Commit inicial

---
+++ Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/test/java/br/com/torugo/primeiroprojetospring/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.torugo.primeiroprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
