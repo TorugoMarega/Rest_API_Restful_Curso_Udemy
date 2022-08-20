98ebe16f10e6b0c11d5ba3f1a2ca46579f379302
Vhmarega <victorhugomarega@hotmail.com>
2022-08-19 22:20:05 -0300

    Aula 57 e 58: Implementando operações de subtração, multiplicação e divisão

--- Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/MathController.java
+++ Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/MathController.java
@@ -18,7 +18,29 @@ public class MathController {
             }
             return Double.parseDouble(numberOne)+Double.parseDouble(numberTwo);
     }
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sub(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return Double.parseDouble(numberOne)-Double.parseDouble(numberTwo);
+    }
 
+    @RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double mult(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return Double.parseDouble(numberOne)*Double.parseDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double div(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return Double.parseDouble(numberOne)/Double.parseDouble(numberTwo);
+    }
     private boolean isNumeric(String strNumber){
         if(strNumber==null) return false;
         String number = strNumber.replaceAll(",",".");
