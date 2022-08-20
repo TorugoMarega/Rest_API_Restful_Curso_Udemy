d695d2c4fc8398fdde283091958ad7855d769b0f
Vhmarega <victorhugomarega@hotmail.com>
2022-08-19 21:23:02 -0300

    Aula 55

--- Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/MathController.java
+++ Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/MathController.java
@@ -19,6 +19,15 @@ public class MathController {
     }
 
     private boolean isNumeric(String strNumber){
-        return false;
+        if(strNumber==null) return false;
+        String number = strNumber.replaceAll(",",".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    private Double convertToDouble(String strNumber){
+        if(strNumber==null) return  0D;
+        String number = strNumber.replaceAll(",",".");
+        if(isNumeric(number)) return Double.parseDouble(strNumber);
+        return 0D;
     }
 }
