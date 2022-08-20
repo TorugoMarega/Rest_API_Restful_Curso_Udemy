b4523758ad52f4f79625dd33560f8291f1531942
Vhmarega <victorhugomarega@hotmail.com>
2022-08-19 23:23:40 -0300

    Aula 58: Implementando operações de potência, raiz quadrada e média

--- Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/MathController.java
+++ Secao5-PrimeirosPassosComSpringboot/rest-with-spring-boot-and-java/src/main/java/br/com/torugo/primeiroprojetospring/MathController.java
@@ -18,29 +18,54 @@ public class MathController {
             }
             return Double.parseDouble(numberOne)+Double.parseDouble(numberTwo);
     }
-    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sub(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double subtraction(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
         return Double.parseDouble(numberOne)-Double.parseDouble(numberTwo);
     }
 
-    @RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double mult(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
         return Double.parseDouble(numberOne)*Double.parseDouble(numberTwo);
     }
 
-    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double div(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double division(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
         return Double.parseDouble(numberOne)/Double.parseDouble(numberTwo);
     }
+
+    @RequestMapping(value = "/average/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double average(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return (Double.parseDouble(numberOne)+Double.parseDouble(numberTwo))/2;
+    }
+
+    @RequestMapping(value = "/squareroot/{numberOne}", method = RequestMethod.GET)
+    public Double squareroot(@PathVariable(value = "numberOne") String numberOne) throws Exception{
+        if(!isNumeric(numberOne)){
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return Math.sqrt(Double.parseDouble(numberOne));
+    }
+
+    @RequestMapping(value = "/power/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double squareroot(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return Math.pow(Double.parseDouble(numberOne), Double.parseDouble(numberTwo));
+    }
+
     private boolean isNumeric(String strNumber){
         if(strNumber==null) return false;
         String number = strNumber.replaceAll(",",".");
